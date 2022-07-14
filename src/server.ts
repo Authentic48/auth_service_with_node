e79cd9c6e5ec724ebc570790daba0560fffd12d8
Authentic48 <iboufall48@gmail.com>
2022-07-14 14:43:22 +0700

    DB connection setup done

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db';
 
+// .env configuration
 dotenv.config()
+
+// DB connection
+connectDB()
 
 const app = express();
 
